2bfac9c2be3535f94dff5b7b79f39f0bc660881e
viranmalaka <viran.malaka@amusedgroup.com>
2023-08-24 22:04:33 +0530

    Initial commit

---
+++ apps/school-mfe-host/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'school-mfe-host',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/school-mfe-host',
+};
